6af42dee1d57a299c3c13b0cdfdb3253b9222369
hodge-py <105604814+hodge-py@users.noreply.github.com>
2023-12-02 12:13:49 -0700

    Update question_2.py

--- Assignment 5/question_2.py
+++ Assignment 5/question_2.py
@@ -3,10 +3,13 @@ file = open('employeedb.txt', 'w')
 records = int(input("Number of Records to enter "))
 
 for x in range(records):
-    input("Enter Name ")
-    input("Enter Age ")
-    input("Enter EmployeedID ")
-    input("Enter Years of Service ")
+    name = input("Enter Name ")
+    for x in name:
+        if x not in range(48,58):
+            pass
+    age = input("Enter Age ")
+    id = input("Enter EmployeedID ")
+    service = input("Enter Years of Service ")
 
 
     print(f"Record {x+1} Entry Successful")
