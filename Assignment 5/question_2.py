3f16e527478a16ec5e1f206423a54097714da15c
hodge-py <105604814+hodge-py@users.noreply.github.com>
2023-12-04 14:50:58 -0700

    Update question_2.py

--- Assignment 5/question_2.py
+++ Assignment 5/question_2.py
@@ -1,17 +1,59 @@
-file = open('employeedb.txt', 'w')
-
-records = int(input("Number of Records to enter "))
-
-for x in range(records):
+def nameCheck():
     name = input("Enter Name ")
     for x in name:
-        if x not in range(48,58):
-            pass
+        if ord(x) not in range(65,123):
+            return ''
+    return name
+
+def ageCheck():
     age = input("Enter Age ")
-    id = input("Enter EmployeedID ")
+    print(age)
+    for x in age:
+
+        if ord(x) not in range(48,58):
+            return ''
+    return age
+
+def idCheck():
+    id = input("Enter ID ")
+    for x in id:
+        if ord(x) not in range(48,58):
+            return ''
+    return id
+
+def serviceCheck():
     service = input("Enter Years of Service ")
+    for x in service:
+        if ord(x) not in range(48,58):
+            return ''
+    return service
 
 
-    print(f"Record {x+1} Entry Successful")
+def main():
+    name = ''
+    age = ''
+    id = ''
+    service = ''
+    file = open('employeedb.txt', 'w')
+
+    records = int(input("Number of Records to enter "))
+
+    for x in range(records):
+        while name == '':
+            name = nameCheck()
+
+        while age == '':
+            age = ageCheck()
+
+        while id == '':
+            id = idCheck()
+        
+        while service == '':
+            service = serviceCheck()
 
 
+        print(f"Record {x+1} Entry Successful")
+        print(name,age,id,service)
+
+
+main()
