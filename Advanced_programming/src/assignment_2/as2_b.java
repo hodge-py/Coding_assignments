898c742458c41bd0e172bc7c52c52f040d8e08c5
hodge-py <105604814+hodge-py@users.noreply.github.com>
2024-02-04 15:52:42 -0700

    comments

--- Advanced_programming/src/assignment_2/as2_b.java
+++ Advanced_programming/src/assignment_2/as2_b.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 
 class as2_b {
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);
+        Scanner myObj = new Scanner(System.in); // Creates the input object
         System.out.println("Enter a number between 11 and 1900");
         int number = 0;
         int range = 0;
-        while(range == 0){
+        while(range == 0){ // Loop breaks once a valid number is inputted
             number = myObj.nextInt();
             if(number < 11 || number > 1900){
                 System.out.println("Out of the range, please enter a number between 11 and 1900");
@@ -22,17 +22,17 @@ class as2_b {
 
     }
 
-    static void primeFactors(int num) {
+    static void primeFactors(int num) { // function to collect the prime factors
         int i = 2;
         String total = "";
         int powCount = 0;
         int tmpnum = num;
-        while (i <= num){
+        while (i <= num){ // loops until i is the same value as num
             if(num % i == 0){
                 powCount += 1;
                 num = num / i;
                 if(num % i != 0){
-                    if(total == ""){
+                    if(total == ""){ // if empty the asterisk is not needed.
                         total += "" + i + "^" + powCount;
                         powCount = 0;
                     }
@@ -43,11 +43,11 @@ class as2_b {
                 }
             }
             else{
-                i = i + 1;
+                i = i + 1; // increases i by 1
             }
 
         }
-        System.out.println(tmpnum + "=" + total);
+        System.out.println(tmpnum + "=" + total); // Prints the total
     }
 
 }
