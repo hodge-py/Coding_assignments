902b39550ab6a2e69d367e675d6823f3f5421bd8
hodge-py <105604814+hodge-py@users.noreply.github.com>
2024-03-02 19:23:14 -0700

    cahnges

--- Advanced_programming/src/assignment_6/SixPartB.java
+++ Advanced_programming/src/assignment_6/SixPartB.java
@@ -4,6 +4,15 @@ public class SixPartB {
 
     public static void main(String[] args) {
 
+        Scanner scanObj = new Scanner(System.in);
+
+        int rows = scanObj.nextInt();
+
+        PascalTriangle pascalObj = new PascalTriangle(rows);
+
+        String finalString = pascalObj.createPascal();
+
+        System.out.println(finalString);
     }
 
 }
@@ -16,6 +25,39 @@ class PascalTriangle {
         mainValue = value;
     }
 
+
+    static int factorial(int fact){
+        int factHold = 1;
+        for (int i = 0; i < fact; i++) {
+            factHold *= (i+1);
+        }
+
+        return factHold;
+    }
+
+
+    public String createPascal(){
+        int lineAssign = 1;
+        int numOfLines = mainValue;
+        String masterString = "";
+        for (int i = 0; i < numOfLines; i++) {
+            int counter = 0;
+            String value = "";
+            while(counter < lineAssign){
+
+                value += String.valueOf(factorial(i) / (factorial(counter)*factorial((i-counter)))) + " ";
+
+
+                counter += 1;
+            }
+
+            masterString += value + "\n";
+
+            lineAssign += 1;
+        }
+
+        return masterString;
+    }
 
 
 
