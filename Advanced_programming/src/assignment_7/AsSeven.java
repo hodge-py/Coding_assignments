5818499afa478710c009ce0c87c0a5462935c306
hodge-py <105604814+hodge-py@users.noreply.github.com>
2024-03-04 15:59:17 -0700

    changes

---
+++ Advanced_programming/src/assignment_7/AsSeven.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+// Karson Hodge, MCIS-Adv Programming Concepts, Section 34, ID: 9999-03235
+public class AsSeven {
+
+    public static void main(String[] args) {
+        Scanner scanObj = new Scanner(System.in);
+
+    }
+
+}
+
+
+
+class RomanConversion {
+
+    RomanConversion (String object){
+        
+    }
+}
