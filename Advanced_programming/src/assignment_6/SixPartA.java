2508aee1fd251be59bcff2edc086ec9dd60d2749
hodge-py <105604814+hodge-py@users.noreply.github.com>
2024-03-04 14:32:01 -0700

    comments

--- Advanced_programming/src/assignment_6/SixPartA.java
+++ Advanced_programming/src/assignment_6/SixPartA.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 
 
 /**
- * This class takes an input and then intializes the LatinString class
+ * This class takes an input and then initializes the LatinString class
  *
- *
+ * @author Karson Hodge
  */
 public class SixPartA {
 
     public static void main(String[] args) {
-        Scanner scanObj = new Scanner(System.in);
-        System.out.println("Input a sentence");
-        String tmpLatin = scanObj.nextLine();
+        Scanner scanObj = new Scanner(System.in); // create new scanner object
+        System.out.println("Input a sentence, english or fake latin");
+        String tmpLatin = scanObj.nextLine(); // user enters the number of rows, is a string
 
         LatinString latinObj = new LatinString(tmpLatin);
-        latinObj.Checker();
+        latinObj.Checker(); // Function used to check if it is fake latin or english
 
     }
 
@@ -26,9 +26,10 @@ public class SixPartA {
  * The constructor handles the original input from the user
  */
 class LatinString {
-    private String word;
-    LatinString(String main) {
+    private String word; // instance variable to hold the input
+    LatinString(String main) { // constructor variable to set the instance variable
         word = main;
+
     }
 
     /**
@@ -37,103 +38,104 @@ class LatinString {
      */
     void Checker(){
         //int len = word.length();
-        boolean latin = true;
+        boolean latin = true; // boolean value that determines if it is fake latin
         String[] arrayS = word.split(" ");
 
-        for (int i = 0; i < arrayS.length; i++) {
+        for (int i = 0; i < arrayS.length; i++) { // Loops through the split array
             if (arrayS[i].charAt(arrayS[i].length()-1) == 'y' && arrayS[i].charAt(arrayS[i].length()-2) == 'a'){
 
             }
-            else{
+            else{ // if it does not follow the convention, latin false, and break loop
                 latin = false;
                 break;
             }
         }
 
 
-        if (latin){
+        if (latin){ // if latin is true
 
             System.out.println("latin");
-            String finalWord = latinToEnglish(word);
+            String finalWord = latinToEnglish(word); // runs the latin to english function
             System.out.println(finalWord);
 
         }
         else{
 
             System.out.println("English");
-            String finalWord = englishToLatin(word);
+            String finalWord = englishToLatin(word); // runs the english to latin function
             System.out.println(finalWord);
 
         }
     }
 
     /**
+     * Converts fake latin to english
      *
      * @param convert String inputted from the intialized variable. This holds the input given by the user.
      * @return finalHold String containing the converted sentence from fake latin to english.
      */
     static String latinToEnglish(String convert){
-        String finalHold = "";
+        String finalHold = ""; // holds the string that will be returned
         String[] latinArr = convert.split(" ");
 
         for (int y = 0; y < latinArr.length; y++) {
-            String tmpHold = "";
+            String tmpHold = ""; // temporary string holder that will add to the final string
             for (int x = 0; x < latinArr[y].length(); x++) {
-                if (x == 0){
+                if (x == 0){ // if zero, add the third last character then add the current x charter
                     tmpHold = tmpHold + latinArr[y].charAt(latinArr[y].length()-3);
                     tmpHold = tmpHold + latinArr[y].charAt(x);
                 }
                 else if (x == latinArr[y].length()-2){
-                    tmpHold = tmpHold.substring(0,tmpHold.length()-1);
+                    tmpHold = tmpHold.substring(0,tmpHold.length()-1); // substring with the last character removed to create a correct string
                     break;
                 }
                 else{
-                    tmpHold = tmpHold + latinArr[y].charAt(x);
+                    tmpHold = tmpHold + latinArr[y].charAt(x); // adds the current character value to the tmpstring
                 }
 
             }
 
-            finalHold += tmpHold + " ";
+            finalHold += tmpHold + " "; // adds temporary string to the final string value
         }
 
-        return finalHold;
+        return finalHold; // returns the final string value
     }
 
     /**
-     *
+     * Converts an english sentence to fake latin
      *
      * @param convert String inputted from the intialized variable. This holds the input given by the user.
      * @return finalHold String containing the converted sentence from english to fake latin
      */
     static String englishToLatin(String convert){
-        String finalHold = "";
+        String finalHold = ""; // temporary string holder that will add to the final string
         String[] latinArr = convert.split(" ");
 
         for (int y = 0; y < latinArr.length; y++) {
-            String tmpHold = "";
+            String tmpHold = ""; // temporary string holder that will add to the final string
             for (int x = 0; x < latinArr[y].length(); x++) {
-                if (x == 0 && latinArr[y].length() == 1){
+                if (x == 0 && latinArr[y].length() == 1){ // if zero and length of word is 1, add 'a' and then 'y' and then break the loop
                     tmpHold = tmpHold + latinArr[y].charAt(0) + 'a' + 'y';
                     break;
                 }
-                else if (x == 0){
+                else if (x == 0){ // do nothing
 
                 }
-                else if ((x+1) == latinArr[y].length()){
+                else if ((x+1) == latinArr[y].length()){ // final character, add 'a' and 'y'
                     tmpHold = tmpHold + latinArr[y].charAt(x) + latinArr[y].charAt(0) + 'a' + 'y';
                 }
-                else{
+                else{ // simply adds the character with no changes
                     tmpHold = tmpHold + latinArr[y].charAt(x);
                 }
 
 
             }
 
-            finalHold += tmpHold + " ";
+            finalHold += tmpHold + " "; // adds the temporary variable to the final string value
         }
 
 
-        return finalHold;
+        return finalHold; // returns the final string variable
     }
 
 }
--- Advanced_programming/src/assignment_6/SixPartB.java
+++ Advanced_programming/src/assignment_6/SixPartB.java
@@ -1,16 +1,22 @@
 import java.util.Scanner;
 // Karson Hodge, MCIS-Adv Programming Concepts, Section 34, ID: 9999-03235
+
+/**
+ * This classes contains the main function, and creates an instance of PascalTriangle
+ *
+ * @author Karson Hodge
+ */
 public class SixPartB {
 
     public static void main(String[] args) {
 
-        Scanner scanObj = new Scanner(System.in);
-
-        int rows = scanObj.nextInt();
+        Scanner scanObj = new Scanner(System.in); // new Scanner object for input
+        System.out.println("Input the number of rows");
+        int rows = scanObj.nextInt(); // receives the users input
 
         PascalTriangle pascalObj = new PascalTriangle(rows);
 
-        String finalString = pascalObj.createPascal();
+        String finalString = pascalObj.createPascal(); // function to create the triangle
 
         System.out.println(finalString);
     }
@@ -19,50 +25,59 @@ public class SixPartB {
 
 
 class PascalTriangle {
-    private int mainValue;
+    private int mainValue; // instance variable
 
     PascalTriangle(int value){
-        mainValue = value;
+        mainValue = value; // sets the instance variable from the constructor
     }
 
-
-    static int factorial(int fact){
+    /**
+     * Factorial function
+     *
+     * @param fact number for factorial
+     * @return factHold int value containing the factorial
+     */
+    static int factorial(int fact){ // simple function to calculate factorials
         int factHold = 1;
-        for (int i = 0; i < fact; i++) {
+        for (int i = 0; i < fact; i++) { // loops through and adds the number of factors
             factHold *= (i+1);
         }
 
-        return factHold;
+        return factHold; // returns the factorial
     }
 
-
+    /**
+     * function that creates the pascal triangle
+     *
+     * @return masterString String variable that holds the full pascal triangle
+     */
     public String createPascal(){
-        int lineAssign = 1;
+        int lineAssign = 1; // sets the row number
         int numOfLines = mainValue;
         String masterString = "";
         for (int i = 0; i < numOfLines; i++) {
-            int counter = 0;
+            int counter = 0; // this sets the column number
             String value = "";
-            while(counter < lineAssign){
+            while(counter < lineAssign){ // continues until the column number equals row number
 
-                value += String.valueOf(factorial(i) / (factorial(counter)*factorial((i-counter)))) + " ";
+                value += String.valueOf(factorial(i) / (factorial(counter)*factorial((i-counter)))) + " "; // binomial coefficient function that creates the triangle values
 
 
-                counter += 1;
+                counter += 1; // adds one to column variable
             }
 
             String tmpString = "";
-            for (int j = 0; j < (numOfLines-i); j++) {
+            for (int j = 0; j < (numOfLines-i); j++) { // adds the space needed to make the triangle
                     tmpString += " ";
             }
 
-            masterString += tmpString + value + "\n";
+            masterString += tmpString + value + "\n"; // adds the space, values, and then a line break
 
             lineAssign += 1;
 
         }
 
-        return masterString;
+        return masterString; // returns the full string
 
     }
 
