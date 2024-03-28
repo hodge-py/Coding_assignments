77d8a395891487c4c7a2e429024f79fdb110e673
hodge-py <105604814+hodge-py@users.noreply.github.com>
2024-03-27 18:59:55 -0600

    changes

--- Advanced_programming/src/assignment_9/NineA.java
+++ Advanced_programming/src/assignment_9/NineA.java
@@ -1,28 +1,60 @@
 // Karson Hodge, MCIS-Adv Programming Concepts, Section 34, ID: 9999-03235
 import java.awt.*;
 import javax.swing.*;
+import java.util.Random;
 
+/**
+ * This class defines the window that the triangles will be drawn on
+ *
+ * @author Karson Hodge
+ */
 public class NineA extends Canvas {
 
     public static void main(String[] args) {
-        var panel = new Draw();
-        panel.setBackground(Color.GREEN.darker());
-        var frame = new JFrame("A simple graphics program");
-        frame.setSize(400, 300);
+        var panel = new Draw(); // class being used to draw
+        var frame = new JFrame("Triangles"); // makes the jframe and puts a title
+        frame.setSize(680, 800); // sets the width and height of the screen
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.getContentPane().add(panel, BorderLayout.CENTER);
-        frame.setVisible(true);
+        frame.setVisible(true); // show the frame
 
     }
 }
 
 
+/**
+ * Handles the drawing of the 500 triangles.
+ *
+ * @author Karson Hodge
+ */
 class Draw extends JPanel {
 
+    /**
+     * This function draws the triangles. It does so by using the graphics library
+     *
+     * @param g the <code>Graphics</code> object to protect
+     * @author Karson Hodge
+     */
     public void paintComponent(Graphics g) {
-        g.setColor(Color.BLUE);
-        g.drawLine(100, 100, 200, 200);
-        g.draw3DRect(100,100,150,100,true);
+
+        Random rand = new Random(); // Creates the random object
+
+        for (int i = 0; i < 500; i++) {
+
+            Color c = new Color(rand.nextInt(256), rand.nextInt(256), rand.nextInt(256)); // randomly chooses a color
+            int height1 = rand.nextInt(1000)+1;
+            int width1 = rand.nextInt(1000)+1;
+            int height2 = rand.nextInt(1000)+1; // each of these deals with a random width or height
+            int width2 = rand.nextInt(1000)+1;
+            int height3 = rand.nextInt(1000)+1;
+            int width3 = rand.nextInt(1000)+1;
+            g.setColor(c); // sets the color
+            g.drawPolygon(new int[] {width1,width2,width3}, new int[] {height1,height2,height3}, 3); // draws the triangle onto the frame.
+        }
+
+
+
+
     }
 
 
